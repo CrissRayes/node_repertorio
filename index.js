f31194a273cf65465e9b8e1ab7d12b809d65ca21
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-05-16 21:28:25 -0400

    CHG: Reading songs from view
    View is getting information from API endpoint successfully

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require( 'fs' )
 const express = require( 'express' )
 const app = express()
 
@@ -5,6 +6,29 @@ const PORT = 3000
 
 app.listen( PORT, console.log( `Servidor encendido en: http://localhost:${ PORT }` ) )
 
+app.use( express.json() )
+
 app.get( "/", ( req, res ) => {
-  res.send( "Hola Mundillo Cruel" )
+  res.sendFile( __dirname + "/index.html" )
+} )
+
+app.post( "/canciones", ( req, res ) => {
+  const song = req.body
+  const songs = JSON.parse( fs.readFileSync( 'repertorio.json' ) )
+  songs.push( song )
+  fs.writeFileSync( 'repertorio.json', JSON.stringify( songs ) )
+  res.send( 'Canción agregada exitósamente' )
+} )
+
+app.get( "/canciones", ( req, res ) => {
+  const songs = JSON.parse( fs.readFileSync( 'repertorio.json' ) )
+  res.json( songs )
+} )
+
+app.put( "/canciones/:id", ( req, res ) => {
+  // algo para editar las canciones
+} )
+
+app.delete( "canciones/:id", ( req, res ) => {
+  // algo para borrar las canciones
 } )
